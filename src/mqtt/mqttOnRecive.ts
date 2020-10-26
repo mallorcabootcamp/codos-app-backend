ee95106cbf160e987b9f3e19f0c1f645abcbde1c
Ralvarez <R.alvgar@hotmail.com>
2020-10-26 16:49:03 +0100

    changes done

--- src/mqtt/mqttOnRecive.ts
+++ src/mqtt/mqttOnRecive.ts
@@ -1,6 +1,6 @@
-import mqtt from 'mqtt';
+import { MqttClient } from 'mqtt';
 
-export const mqttOnRecive = ( client: mqtt.MqttClient) => {
+export const mqttOnRecive = ( client: MqttClient) => {
     client.on('message', (topic, message) => {
       const msgRecived = message.toString();
       console.log(msgRecived)
