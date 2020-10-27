6f069c3c135ce25ef1b70c658273fa85ae1b8a38
Ralvarez <R.alvgar@hotmail.com>
2020-10-27 21:34:11 +0100

    some things more...

--- src/influxDBService/functions/onGetCo2Data.ts
+++ src/influxDBService/functions/onGetCo2Data.ts
@@ -2,5 +2,6 @@
 export function onGetCo2Data(data:any) {
     const eCoIndex = 5;
     const values = data.values.map((e:any) => { return {eCo2: e[eCoIndex]}});
+    console.log(values)
     return values;
 }
--- src/influxDBService/functions/onGetTemperatureData.ts
+++ src/influxDBService/functions/onGetTemperatureData.ts
@@ -1,6 +1,6 @@
-export function onGetTemperatureData(data:any) {
+export function onGetTemperatureData(data: any[]):object[] {
     const temperatureIndex = 3;
-    const values = data.values.map((e:any) => { return {temperature: e[temperatureIndex]}});
+    const values: object[] = data.values.map((e:string) => { return {temperature: e[temperatureIndex]}});
     console.log(values);
     return values;
 }
