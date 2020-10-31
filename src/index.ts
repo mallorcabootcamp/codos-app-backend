1f79b4d99552af814deb34c1ccdf7408478d9ed5
Daniel Nieto <dnietosadurni@gmail.com>
2020-10-31 11:03:18 +0100

    prog

--- src/index.ts
+++ src/index.ts
@@ -15,22 +15,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 app.use(router);
 
-
 app.listen(process.env.PORT, () => {
-     onTry();
      mqttOnConnect();
      console.log('Conectado al puerto ' + process.env.PORT);
 });
-
-async function onTry() {
-     const onConnect = influxDBService.getInstance();
-     await onConnect.connect().then(() => {
-          const co2Data = onConnect.getCo2Data()
-          onConnect.getTemperatureData()
-          onConnect.getHumidityData()
-          onConnect.getDataFromDateToDate({ date: '2020-10-13T19:42:06.784456438Z' }, { date: '2020-10-13T19:48:13.052937184Z' }, co2Data);
-     })
-}
