b3d708c45d4c6e6be7ee42e103d60d9b9dffd006
Ralvarez <R.alvgar@hotmail.com>
2020-11-17 18:56:12 +0100

    new logs for the new controllers

--- src/controllers/userCurrentData.ts
+++ src/controllers/userCurrentData.ts
@@ -1,29 +1,36 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { InfluxDBService, dataResponse } from '../influxDBService/influxDBService';
 import moment from 'moment';
 import { switchDataToGet } from '../utils/switchDataToGet';
+import debug from 'debug';
 
-export const userCurrentData = (req: Request, res: Response) => {
+const log = debug("app:controller:userCurrentData")
+
+export const userCurrentData = (req: Request, res: Response, next: NextFunction) => {
 
      const user = req.query.user;
      const dataToGet = req.query.dataToGet;
+
+     log(`getting ${dataToGet} data`);
 
      const instance = new InfluxDBService();
 
      instance.getUserCurrentData(user as string, switchDataToGet(user as string, dataToGet as string)).then(((data: dataResponse[]):void => {
           try {
+               log(`receiving ${dataToGet} data`);
                res.json(data.map((dataItem: any):dataResponse => ({
                     time: Math.round(moment(dataItem[0]).valueOf() / 1000).toString(),
                     value: dataItem[1]
                })));
-
+               log("exit controller");
           } catch (error) {
-               console.log('ERROR: ', error);
+               log("ERROR: ", error);
                res.status(500).json({
                     ok: false,
                     msg: 'Error de servidor'
                });
           }
+          next();
      }))
 };
 
